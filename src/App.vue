ae44d63c6bb5de344ff0ec2546db4685594ae882
lucasens1 <lucasensini1@gmail.com>
2024-05-07 15:38:59 +0200

    Added, Store, Array took by Store.js and AppSearch.vue

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,46 @@
   import axios from "axios";
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
+  import AppSearch from './components/AppSearch.vue';
+  import { store } from "./store";
 
   export default {
     components: {
       AppHeader,
+      AppSearch,
       AppMain
     },
     data(){
       return {
         //Creo un Array che ospiterà le informazioni sui pg di Rick&Morty
         charactersArray : [],
+        store,
       };
     },
     created(){
-      //All'avvio della pagina prendo i dati dal server
-      axios.get('https://rickandmortyapi.com/api/character')
-      .then((resp) => {
-        console.log(resp);
-        console.log(resp.data.results);
-        this.charactersArray = resp.data.results;
-        console.log(this.charactersArray);
-      })
-    }
-  }
+      this.getStatus();
+    },
+    methods : {
+      getStatus(){
+        console.log("Get Status richiamato correttamente");
+        //Creo un oggetto con status null che prende il valore del selected status quando è diverso da All
+        const paramsObject = {
+          status : null,
+        }
+        //Se ho selectedStatus diverso da All (situazione iniziale) assegno lo status alla variabile dentro l'oggetto creato
+        if(this.store.selectedStatus !== 'All'){
+          paramsObject.status = this.store.selectedStatus;
+        }
+        //Chiamata API
+        axios.get('https://rickandmortyapi.com/api/character',
+                 {params : paramsObject} ).then((resp) => {
+                                                            console.log("Assegnando valori all'Array . . . ")
+                                                            this.store.characterList = resp.data.results;
+                                                            console.log("Array dello store, aggiornato : ", this.store.characterList);
+                                                          });
+      },
+    },
+  };
 </script>
 
 <template>
@@ -32,9 +49,11 @@
   <div class="container-fluid">
     <!-- Header -->
     <AppHeader />
+    <!-- SearchBar @statusFilter permette la scrematura per Status -->
+    <AppSearch @statusFilter="getStatus"/>
     <!-- Main -->
-    <!-- Passo l'array al Main -->
-    <AppMain :charArray="charactersArray" /> 
+    <!-- Passo l'array dallo Store al Main -->
+    <AppMain :charArray="store.characterList" /> 
   </div>
 
 </template>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from "../store";
+export default {
+  data() {
+    return {
+      store,
+      archestatus: ["All", "Alive", "Dead", "Unknown"]
+    };
+  },
+};
+</script>
+
+<template>
+    <div class="text-center p-2 mb-2"> 
+        <!-- Lo status selezionato viene passato allo store tramite v-model -->
+        <select 
+            @change="$emit('statusFilter')"
+            v-model="store.selectedStatus"
+            class="p-2 fs-6 border-0 rounded-4">
+            <!-- Stampo i possibili Status dei personaggi-->
+            <option :value="archestat" v-for="archestat in archestatus">
+                {{ archestat }}
+            </option>
+        </select>
+    </div>
+</template>
